5a1dae25c115d7a06dc0a99ea30a058f0491dbe0
Anish Mondal <mondalanish10@gmail.com>
2023-09-03 23:31:02 +0530

    Initial commit

---
+++ first_program.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str ;
+    cout <<"enter the word: ";
+    getline(cin, str);
+    cout << "The entered word is: " << str;
+
+    return 0;
+}
---
+++ prog_002.cpp
@@ -0,0 +1,13 @@
+// User Input in C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter the String: ";
+    getline(cin, str);
+    cout << str;
+
+    return 0;
+}
